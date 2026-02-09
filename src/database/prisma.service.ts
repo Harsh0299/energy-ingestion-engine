c267bade69427d194d218ec92eaeb9c5171121a7
Harsh0299 <harsh021999@gmail.com>
2026-02-10 01:57:47 +0530

    downgraded prisma

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,33 +1,13 @@
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const connectionString = process.env.DATABASE_URL;
-    if (!connectionString) {
-      throw new Error('DATABASE_URL environment variable is required');
-    }
-    super({
-      adapter: new PrismaPg({
-        connectionString,
-        ssl: { rejectUnauthorized: false },
-        pool: {
-          max: 3,
-          idleTimeoutMillis: 10000,
-        },
-      })
-    });
+    super({});
   }
 
   async onModuleInit() {
     await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    // await this.$disconnect();
   }
 }
